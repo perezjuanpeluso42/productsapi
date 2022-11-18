fbfe867726d6e2219e54a0896b4c076622d6cf68
pancho <perezjuanpeluso33@gmail.com>
2022-11-18 20:38:59 -0300

    cambie titulo a producto

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Products',
+      title: 'Productos',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColorLight: Colors.white,
